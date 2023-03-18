8401e637e0242a99aef6276432c7837f5c07ed56
OrlandoCodes10 <orlandofranco.novesteras@gmail.com>
2023-03-18 03:00:48 -0400

    Completed first part of classes practice

--- 4_JavaScriptClasses/app.js
+++ 4_JavaScriptClasses/app.js
@@ -81,9 +81,26 @@ class Color {
         this.b = b;
         this.name = name;
     }
+    innerRGB() {
+        const { r, g, b } = this;
+        return `rgb(${r}, ${g}, ${b})`;
+    }
+    rgb() {
+        return `rgb(${this.innerRGB()}`;
+    }
+    rgba(a = 1.0) {
+        return `rgba(${this.innerRGB()}, ${a})`;
+    }
+    hex() {
+        const { r, g, b } = this;
+        return (
+            '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)
+        );
+    }
 }
 
-const c1 = new Color(255, 67, 89, 'tomato');
+const red = new Color(255, 67, 89, 'tomato');
+const white = new Color(255, 255, 255, 'white');
 
 
 //Constructor and classes functions need to start with Uppercase
