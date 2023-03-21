3fefbc56618191aa7885f5bdf73ba7445a063f8c
OrlandoCodes10 <orlandofranco.novesteras@gmail.com>
2023-03-20 22:44:25 -0400

    Semi Complete

--- 5_JavaScriptClassesMore/app.js
+++ 5_JavaScriptClassesMore/app.js
@@ -21,6 +21,48 @@ class Color {
             '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)
         );
     }
+    calcHSL() {
+        let { r, g, b } = this;
+        // Make r, g, and b fractions of 1
+        r /= 255;
+        g /= 255;
+        b /= 255;
+    
+        // Find greatest and smallest channel values
+        let cmin = Math.min(r, g, b),
+            cmax = Math.max(r, g, b),
+            delta = cmax - cmin,
+            h = 0,
+            s = 0,
+            l = 0;
+        if (delta == 0) h = 0;
+        else if (cmax == r)
+            // Red is max
+            h = ((g - b) / delta) % 6;
+        else if (cmax == g)
+            // Green is max
+            h = (b - r) / delta + 2;
+        else
+            // Blue is max
+            h = (r - g) / delta + 4;
+    
+        h = Math.round(h * 60);
+    
+        // Make negative hues positive behind 360°
+        if (h < 0) h += 360;
+        // Calculate lightness
+        l = (cmax + cmin) / 2;
+    
+        // Calculate saturation
+        s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+    
+        // Multiply l and s by 100
+        s = +(s * 100).toFixed(1);
+        l = +(l * 100).toFixed(1);
+        this.h = h;
+        this.s = s;
+        this.l = l;
+    }
 }
 
 const red = new Color(255, 67, 89, 'tomato');
@@ -31,46 +73,5 @@ const white = new Color(255, 255, 255, 'white');
 //Constructor functions will execute the constructor once we add 
 //a new color
 
-calcHSL() {
-    let { r, g, b } = this;
-    // Make r, g, and b fractions of 1
-    r /= 255;
-    g /= 255;
-    b /= 255;
 
-    // Find greatest and smallest channel values
-    let cmin = Math.min(r, g, b),
-        cmax = Math.max(r, g, b),
-        delta = cmax - cmin,
-        h = 0,
-        s = 0,
-        l = 0;
-    if (delta == 0) h = 0;
-    else if (cmax == r)
-        // Red is max
-        h = ((g - b) / delta) % 6;
-    else if (cmax == g)
-        // Green is max
-        h = (b - r) / delta + 2;
-    else
-        // Blue is max
-        h = (r - g) / delta + 4;
-
-    h = Math.round(h * 60);
-
-    // Make negative hues positive behind 360°
-    if (h < 0) h += 360;
-    // Calculate lightness
-    l = (cmax + cmin) / 2;
-
-    // Calculate saturation
-    s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
-
-    // Multiply l and s by 100
-    s = +(s * 100).toFixed(1);
-    l = +(l * 100).toFixed(1);
-    this.h = h;
-    this.s = s;
-    this.l = l;
-}
 "hsl(130, 50%, 80%"
